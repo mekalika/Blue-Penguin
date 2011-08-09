222c65d95f22e0500cb4f63a65a0b13ae43cb887
Sarah Nguyen <mekalika@gmail.com>
2011-08-08 20:26:57 -0700

    Fixed traits so header refreshes

--- traits.php
+++ traits.php
@@ -50,12 +50,56 @@ function allocateSkillPoint(playerID, trait)
       // status == 200 means the query returned successfully
       if (this.status == 200) {
         if (this.responseText != null) {
+          // Refresh header
+          refreshHeader()
+
           // Refresh traits list
           getTraits(playerID)
         }
         else alert("Ajax error: No data received")
       }
       else alert("Ajax error1: " + this.statusText + " " + this.status)
+    }
+  }
+  request.send(params)
+}
+
+function refreshHeader()
+{
+  params = ""
+  request = new ajaxRequest()
+  request.open("POST", "refresh_header.php", true)
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  request.setRequestHeader("Content-length", params.length)
+  request.setRequestHeader("Connection", "close")
+
+  request.onreadystatechange = function()
+  {
+    if (this.readyState == 4) // readyState == 4 means the page is done loading
+    {
+      if (this.status == 200) // status == 200 means the query returned successfully
+      {
+        if (this.responseXML != null)
+        {
+          // Parse XML data
+          currMotivation        = this.responseXML.getElementsByTagName('currmotivation')
+          maxMotivation         = this.responseXML.getElementsByTagName('maxmotivation')
+          currPride             = this.responseXML.getElementsByTagName('currpride')
+          maxPride              = this.responseXML.getElementsByTagName('maxpride')
+          cash                  = this.responseXML.getElementsByTagName('cash')
+          gradeString           = this.responseXML.getElementsByTagName('gradestring')
+          
+          // Update ID card info
+          document.getElementById('currMotivation').innerHTML = currMotivation[0].childNodes[0].nodeValue
+          document.getElementById('maxMotivation').innerHTML = maxMotivation[0].childNodes[0].nodeValue
+          document.getElementById('currPride').innerHTML = currPride[0].childNodes[0].nodeValue
+          document.getElementById('maxPride').innerHTML = maxPride[0].childNodes[0].nodeValue
+          document.getElementById('cash').innerHTML = cash[0].childNodes[0].nodeValue
+          document.getElementById('gradeString').innerHTML = gradeString[0].childNodes[0].nodeValue
+        }
+        else alert("Ajax error: No data received")
+      }
+      else alert("Ajax error2: " + this.statusText + " " + this.status)
     }
   }
   request.send(params)
